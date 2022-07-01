89f0fe18d2cbf040459598abe94fd846c53de5d7
Amit <aubhi3@gatech.edu>
2022-07-01 09:11:46 -0400

    Update data_cleaning.R

--- Code/data_cleaning.R
+++ Code/data_cleaning.R
@@ -29,6 +29,20 @@ df.weather.clean$created_at <- df.weather.clean$datetime
 df.new = merge(x=df.order.date, y=df.weather.clean, by="created_at")
 
 #cleaning orders data
-orders_clean = df.orders[-c(5:10,12,15:21,23,24,26)]
-View(orders_clean)
+orders_clean = df.orders[-c(5:10,12,15:21,24,26)]
+dim(orders_clean)
 
+#item_Count column - Removed all rows associated with blank cells (N/A) in item count. 
+#Removed 5.1% of the data. I.e From 135303 columns to 128378
+orders_clean = orders_clean[!(is.na(orders_clean$item_count) | orders_clean$item_count==""), ]
+dim(orders_clean)
+
+#vendor_rating has 86710 NA columns. Should we replace with -1?
+summary(orders_clean$vendor_rating)
+
+#preparation_time has 48635 NA columns. Should we replace with -1?
+summary(orders_clean$preparationtime)
+
+#preparation_time has 48635 NA columns. Should we replace with -1?
+summary(orders_clean$created_at)
+View(orders_clean)
